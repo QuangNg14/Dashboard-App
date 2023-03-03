8ad29999f8632faea7ec9c0ae61d21412e3a8ee4
Quang Nguyen <39874396+QuangNg14@users.noreply.github.com>
2023-03-03 11:31:14 -0600

    Update googleReverseGeocoding.js

--- src/api/googleReverseGeocoding.js
+++ src/api/googleReverseGeocoding.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
-const API_KEY = "AIzaSyA7DUp8WVvBeE7to9pjYRTKxfsWHVxDwA8"
+const BASE_URL = ""
+const API_KEY = ""
 const reverseGeocodeAPI = async (latlng) => {
   const res = await axios.get(`${BASE_URL}?latlng=${latlng}&key=${API_KEY}`)
   return res.data.results[0].formatted_address
